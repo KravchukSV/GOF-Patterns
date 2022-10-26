0372bf9934899600b911a0bc406e4492042fceab
Serhii <kravchuk.s.v.1@gmail.com>
2022-10-26 18:22:41 +0300

    Add Test

---
+++ src/com/example/patterns/Test.java
@@ -0,0 +1,16 @@
+package com.example.patterns;
+
+public class Test {
+    public static void main(String[] args) {
+        int result = 0;
+        int a = 2;
+        int b = 3;
+
+        result = a + b;
+
+        result *= a;
+
+        System.out.println(result);
+
+    }
+}
