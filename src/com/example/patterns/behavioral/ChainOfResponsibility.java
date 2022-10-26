fe9fb544a2d25c234b0004b1f5872935dcc16a41
Serhii <kravchuk.s.v.1@gmail.com>
2022-10-26 20:58:36 +0300

    Delete sout

--- src/com/example/patterns/behavioral/ChainOfResponsibility.java
+++ src/com/example/patterns/behavioral/ChainOfResponsibility.java
@@ -6,7 +6,7 @@ import com.example.patterns.behavioral.cor.ErrorWebsite;
 
 public class ChainOfResponsibility {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
         ErrorWebsite connectionError = new ConnectionError(3);
         ErrorWebsite authorizationError = new AuthorizationError(5);
 
